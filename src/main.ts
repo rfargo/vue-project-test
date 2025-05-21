029356498a4ab101129391417123889cf83fe6be
rfargo <renaldy_fargo@yahoo.com>
2025-05-21 18:37:01 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+/* eslint-disable prefer-rest-params */
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { loadGtm } from './utils/gtm'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +14,7 @@ app.use(router)
 
 app.mount('#app')
 
+// Push default denied consent before anything else
 window.dataLayer = window.dataLayer || []
 window.dataLayer.push({
   event: 'default_consent',
@@ -26,11 +27,18 @@ window.dataLayer.push({
   },
 })
 
-window.gtag?.('consent', 'default', {
+window.gtag =
+  window.gtag ||
+  function () {
+    Array.prototype.forEach.call(arguments, (arg) => window.dataLayer.push(arg))
+  }
+
+window.gtag('consent', 'default', {
   ad_user_data: 'denied',
   ad_personalization: 'denied',
   ad_storage: 'denied',
   analytics_storage: 'denied',
+  security_storage: 'denied',
   wait_for_update: 500,
 })
 
@@ -42,14 +50,11 @@ if (stored) {
     window.dataLayer.push({
       event: 'default_consent',
       consent: {
+        analytics_storage: consent.analytics ? 'granted' : 'denied',
+        ad_storage: consent.ads ? 'granted' : 'denied',
+        security_storage: consent.errorTracking ? 'granted' : 'denied',
         error_tracking: consent.errorTracking ? 'granted' : 'denied',
       },
-    })
-
-    window.gtag?.('consent', 'update', {
-      analytics_storage: consent.analytics ? 'denied' : 'denied',
-      ad_storage: consent.ads ? 'denied' : 'denied',
-      security_storage: consent.errorTracking ? 'denied' : 'denied',
     })
 
     const script = document.createElement('script')
@@ -58,10 +63,60 @@ if (stored) {
     script.src = `https://www.googletagmanager.com/gtm.js?id=${import.meta.env.VITE_GAID}`
     document.head.appendChild(script)
 
-    window.dataLayer = window.dataLayer || []
-    window.dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime() })
+    // Wait for gtag to be available before applying consent updates
+    const applyConsent = () => {
+      if (typeof window.gtag === 'function') {
+        window.gtag('consent', 'update', {
+          analytics_storage: consent.analytics ? 'granted' : 'denied',
+          ad_storage: consent.ads ? 'granted' : 'denied',
+          security_storage: consent.errorTracking ? 'granted' : 'denied',
+        })
+
+        // Fire a manual page_view event to ensure GA starts after consent
+        if (consent.analytics) {
+          window.gtag('event', 'page_view', {
+            page_location: window.location.href,
+            page_path: window.location.pathname,
+            page_title: document.title,
+          })
+        }
+      } else {
+        setTimeout(applyConsent, 50)
+      }
+    }
+
+    applyConsent()
   }
 }
+
+// const stored = localStorage.getItem('cookie_consent')
+
+// if (stored) {
+//   const consent = JSON.parse(stored)
+//   if (consent) {
+//     window.dataLayer.push({
+//       event: 'default_consent',
+//       consent: {
+//         error_tracking: consent.errorTracking ? 'granted' : 'denied',
+//       },
+//     })
+
+//     window.gtag?.('consent', 'update', {
+//       analytics_storage: consent.analytics ? 'denied' : 'denied',
+//       ad_storage: consent.ads ? 'denied' : 'denied',
+//       security_storage: consent.errorTracking ? 'denied' : 'denied',
+//     })
+
+//     const script = document.createElement('script')
+//     script.id = 'gtm-script'
+//     script.async = true
+//     script.src = `https://www.googletagmanager.com/gtm.js?id=${import.meta.env.VITE_GAID}`
+//     document.head.appendChild(script)
+
+//     window.dataLayer = window.dataLayer || []
+//     window.dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime() })
+//   }
+// }
 
 // const stored = localStorage.getItem('cookie_consent')
 // if (stored) {
